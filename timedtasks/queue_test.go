74a4e16104c3750229af32c8e1b639ce2b9aa07f
Aleksandr Vinogradov <camaradavinograda@gmail.com>
2025-05-12 23:14:26 +0200

    Tests: remove lame; use channel over atomic for sync

--- timedtasks/queue_test.go
+++ timedtasks/queue_test.go
@@ -3,48 +3,32 @@ package queue
 import (
 	"testing"
 	"time"
-    "sync/atomic"
 )
-
-func TestAdd(t *testing.T) {
-	q := NewQueue()
-
-	task := &mockTask{}
-	timestamp := time.Now().Add(time.Hour)
-
-	q.Add(task, timestamp)
-
-	if len(q.tasks[timestamp]) != 1 {
-		t.Errorf("Expected 1 task in queue, but got %d", len(q.tasks[timestamp]))
-	}
-
-	if q.tasks[timestamp][0] != task {
-		t.Errorf("Expected added task to be in queue, but got different task")
-	}
-}
 
 func TestRun(t *testing.T) {
 	q := NewQueue()
 
-	var executed int32
-	task := &mockTask{executed: &executed}
+	done := make(chan bool)
+	task := &mockTask{done: done}
 	timestamp := time.Now().Add(time.Second)
 
 	q.Add(task, timestamp)
 
 	go q.Run()
 
-	time.Sleep(2 * time.Second)
+	<-done
 
-	if atomic.LoadInt32(&executed) != 1 {
+	if !task.executed {
 		t.Errorf("Expected task to be executed, but it was not")
 	}
 }
 
 type mockTask struct {
-	executed *int32
+	executed bool
+	done     chan bool
 }
 
 func (m *mockTask) Exec() {
-	atomic.AddInt32(m.executed, 1)
+	m.executed = true
+	m.done <- true
 } 
