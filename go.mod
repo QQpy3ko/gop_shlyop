267e4d38a7be332ad6b4ef02ebe8ec49027e4f40
Aleksandr Vinogradov <qqpy3ko@gmail.com>
2025-06-08 18:50:44 +0200

    chi and routes: get, post review, get item rating

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module gop_shlyop
 go 1.24.2
 
 require (
+	github.com/go-chi/chi/v5 v5.2.1
 	github.com/golang-migrate/migrate/v4 v4.18.3
 	github.com/jackc/pgx/v5 v5.7.5
 	github.com/rs/zerolog v1.34.0
--- internal/server/server.go
+++ internal/server/server.go
@@ -5,10 +5,14 @@ import (
 	"fmt"
 	"gop_shlyop/internal/config"
 	"gop_shlyop/internal/db"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/rs/zerolog"
 )
@@ -36,12 +40,38 @@ func New(ctx context.Context, cfg *config.Config, log zerolog.Logger) (*Server, 
 
 // run server
 func (s *Server) Run() error {
+	router := chi.NewRouter()
+
+	// Middleware
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	router.Use(middleware.Logger) // Chi's own logger is fine for now
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.Timeout(60 * time.Second))
+
+	// API routes
+	router.Route("/api/v1", func(r chi.Router) {
+		r.Post("/reviews", s.addReview)
+		r.Get("/reviews", s.getReviews)
+		r.Get("/items/{id}/rating", s.getItemRating)
+	})
+
+	httpServer := &http.Server{
+		Addr:    fmt.Sprintf("%s:%s", s.cfg.HTTPServer.Host, s.cfg.HTTPServer.Port),
+		Handler: router,
+	}
+
 	s.log.Info().
 		Str("host", s.cfg.HTTPServer.Host).
 		Str("port", s.cfg.HTTPServer.Port).
 		Msg("server is starting")
 
-	// TODO: run web-server in separate goroutine
+	// run web-server in separate goroutine
+	go func() {
+		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			s.log.Fatal().Err(err).Msg("failed to start server")
+		}
+	}()
 
 	// graceful shutdown
 	quit := make(chan os.Signal, 1)
@@ -51,8 +81,31 @@ func (s *Server) Run() error {
 
 	s.log.Info().Msg("Shutting down server...")
 
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := httpServer.Shutdown(ctx); err != nil {
+		s.log.Error().Err(err).Msg("Server forced to shutdown")
+	}
+
 	s.dbpool.Close()
 
 	s.log.Info().Msg("Server stopped gracefully")
 	return nil
+}
+
+// TODO: Implement handlers
+func (s *Server) addReview(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+	w.Write([]byte("Not Implemented"))
+}
+
+func (s *Server) getReviews(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+	w.Write([]byte("Not Implemented"))
+}
+
+func (s *Server) getItemRating(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+	w.Write([]byte("Not Implemented"))
 }
